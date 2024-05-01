82f6e7138262007bcd92e213c9e25199a6a99750
RayanMajrashi <rayanmajrshi1@gmail.com>
2024-05-01 13:04:22 +0000

    added register company functions

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template
-from database import load_companies_from_db
+from flask import Flask, render_template, jsonify, request
+from database import load_companies_from_db, load_company_from_db, add_company_to_db
 
 app = Flask(__name__)
 
@@ -9,16 +9,59 @@ def home():
   return render_template("Pages/home.html")
 
 
-@app.route("/RegisterCompany")
+@app.route("/RegisterCompany", methods=['GET', 'POST'])
 def RegisterCompany():
+  if request.method == 'POST':
+    # Get form data
+    company_data = {
+        "name":
+        request.form['name'],
+        "license_number":
+        request.form['license_number'],
+        "commercial_register":
+        request.form['commercial_register'],
+        "owners": [
+            request.form['owner1'], request.form['owner2'],
+            request.form['owner3']
+        ],
+        "start_date":
+        request.form['start_date'],
+        "communication_numbers": [
+            request.form['communication_number1'],
+            request.form['communication_number2'],
+            request.form['communication_number3']
+        ],
+        "email":
+        request.form['email']
+    }
+    result = add_company_to_db(company_data)
+
+    # Check the result of the insertion
+    if result is True:
+      success_message = "Company inserted successfully."
+      return render_template('Pages/RegisterCompany.html',
+                             success_message=success_message)
+    else:
+      error_message = "Failed to insert company: " + str(result)
+      return render_template('Pages/RegisterCompany.html',
+                             error_message=error_message)
+
   return render_template("Pages/RegisterCompany.html")
 
 
 @app.route("/Companies")
 def Companies():
   Companies = load_companies_from_db()
-  return render_template("Pages/Companies.html",
-                         companies=Companies)
+  return render_template("Pages/Companies.html", companies=Companies)
+
+
+@app.route("/Company/<id>")
+def show_Company(id):
+  company = load_company_from_db(id)
+  if not company:
+    return "Not Found", 404
+  # return render_template("Pages/Company.html", company=company)
+  return jsonify(company)
 
 
 if __name__ == "__main__":
--- database.py
+++ database.py
@@ -8,7 +8,6 @@ def load_companies_from_db():
   Companies = []
   try:
     conn = psycopg2.connect(DB_URL)
-    print("Connection to the database successful")
     SQL = "SELECT * FROM Company"
     with conn:
       with conn.cursor(cursor_factory = psycopg2.extras.DictCursor) as curs:
@@ -18,6 +17,42 @@ def load_companies_from_db():
     conn.close()
     return Companies
   except psycopg2.OperationalError as e:
-    print("Unable to connect to the database:", e)
+    return e
     return Companies
-  
+
+def load_company_from_db(id):
+  company = []
+  try:
+    conn = psycopg2.connect(DB_URL)
+    print("Connection to the database successful")
+    SQL = "SELECT * FROM Company where id = %s"
+    data = (id,)
+    with conn:
+      with conn.cursor(cursor_factory = psycopg2.extras.DictCursor) as curs:
+           curs.execute(SQL,data)
+           if curs.rowcount == 0:
+            return None
+           else:
+            for row in curs:
+             company.append(dict(row))  
+    conn.close()
+    return company
+  except psycopg2.OperationalError as e:
+    return e
+    return company
+
+def add_company_to_db(data):
+  try:
+    conn = psycopg2.connect(DB_URL)
+    SQL = "INSERT INTO Company (name, license_number, commercial_register, owners,start_date, communication_numbers, email) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+    data = (data['name'], data['license_number'], data['commercial_register'], data['owners'], data['start_date'], data['communication_numbers'], data['email'])
+    with conn:
+      with conn.cursor(cursor_factory = psycopg2.extras.DictCursor) as curs:
+             curs.execute(SQL,data)
+    # conn.commit()
+    curs.close()
+    conn.close()
+    return True
+  except psycopg2.OperationalError as e:
+      # print("Unable to connect to the database:", e)
+      return False,e
